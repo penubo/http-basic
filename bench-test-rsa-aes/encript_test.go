4c507eb73e50b0078ca269cecbb67d36ccda6c5c
yongjoon <yongjoon@bearrobotics.ai>
2020-12-03 01:41:46 +0900

    added RSA benchmark

---
+++ bench-test-rsa-aes/encript_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"crypto/md5"
+	"crypto/rand"
+	"crypto/rsa"
+	"io"
+	"testing"
+)
+
+func prepareRSA() (sourceData, label []byte, privateKey *rsa.PrivateKey) {
+	sourceData = make([]byte, 128)                     // capacity 128 right?
+	label = []byte("")                                 // what is label
+	io.ReadFull(rand.Reader, sourceData)               //
+	privateKey, _ = rsa.GenerateKey(rand.Reader, 2048) //
+	return
+}
+
+func BenchmarkRSAEncryption(b *testing.B) {
+	sourceData, label, privateKey := prepareRSA() // what is label here
+	publicKey := &privateKey.PublicKey
+	md5hash := md5.New()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		rsa.EncryptOAEP(md5hash, rand.Reader, publicKey, sourceData, label)
+	}
+}
+
+func BenchmarkRSADecryption(b *testing.B) {
+	sourceData, label, privateKey := prepareRSA()
+	publicKey := &privateKey.PublicKey
+	md5hash := md5.New()
+	encrypted, _ := rsa.EncryptOAEP(md5hash, rand.Reader, publicKey, sourceData, label)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		rsa.DecryptOAEP(md5hash, rand.Reader, privateKey, encrypted, label)
+	}
+}
